6c302a64df92aac94583192177ffb5fb837f3236
VahagnVoskanyan <vahagnlvoskanyan@mail.ru>
2022-08-18 14:33:23 +0400

    test

---
+++ MyList/MyList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+    internal class MyList<T>
+    {
+        private T[] arr = new T[2];
+        public T this[int index]
+        {
+            get { return arr[index]; }
+            set { arr[index] = value; }
+        }
+        public int Count { get; private set; } = 0;
+        public void Add(T value)
+        {
+            if (Count < arr.Length)
+            {
+                arr[Count] = value;
+                Count++;
+            }
+            else
+            {
+                ArrIncrease_x2();
+                arr[Count] = value;
+                Count++;
+            }
+        }
+        public void Print()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        private void ArrIncrease_x2()
+        {
+            var newArray = new T[arr.Length * 2];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArray[i] = arr[i];
+            }
+            arr = newArray;
+        }
+    }
+}
---
+++ MyList/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MyList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Print();
+        }
+    }
+}
