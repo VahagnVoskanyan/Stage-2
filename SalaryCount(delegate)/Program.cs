f76fe66ed8ac5cfcd15d3e2a30c4a1844c1f78cf
VahagnVoskanyan <vahagnlvoskanyan@mail.ru>
2022-08-18 13:27:23 +0400

    new

---
+++ SalaryCount(delegate)/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SalaryCount_delegate_
+{
+    internal class Program
+    {
+        public delegate void SalaryCounter(decimal n);
+        static void Main(string[] args)
+        {
+            Salary salary = new Salary();
+            SalaryCounter count = salary.Decreas1;
+            count += salary.Decreas2;
+            count += salary.Decreas3;
+            count += salary.Decreas4;
+            count += salary.Result;
+            count(210_000);
+            salary.ShowDecreases();
+        }
+    }
+}
---
+++ SalaryCount(delegate)/Salary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCount_delegate_
+{
+    internal class Salary
+    {
+        private List<decimal> Decreases = new List<decimal>();
+        public void Decreas1(decimal n)
+        {
+            Decreases.Add(1000);
+        }
+        public void Decreas2(decimal n)
+        {
+            Decreases.Add(n * 7 / 100);
+        }
+        public void Decreas3(decimal n)
+        {
+            Decreases.Add(n * 9 / 100);
+        }
+        public void Decreas4(decimal n)
+        {
+            Decreases.Add(n * 11 / 100);
+        }
+        public void Result(decimal n)
+        {
+            foreach (var item in Decreases)
+            {
+                n -= item;
+            }
+            Console.WriteLine("The real salary is: " + n);
+        }
+        public void ShowDecreases()
+        {
+            Console.WriteLine("Decreases:");
+            foreach (var item in Decreases)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
