8d1c346aae9226aa999485eb225fb8e3e04042df
VahagnVoskanyan <vahagnlvoskanyan@mail.ru>
2022-08-19 15:45:35 +0400

    new

--- FileSystem/Program.cs
+++ FileSystem/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            Directory.CreateDirectory(@"D:\New Folder");
+            if (Directory.Exists(@"D:\Desktop files\QT"))
+            {
+                Console.WriteLine("true");
+            }
+            string[] str = Directory.GetDirectories(@"D:\Desktop files\QT");
+            foreach (var item in str)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
---
+++ Threads/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread myThread1 = new Thread(Print1); //ThreadStart delegate
+            //Thread myThread1 = new Thread(new ThreadStart(Print1)); //Same as above
+            Thread myThread2 = new Thread(Print2); //ParameterizedThreadStart delegate
+            //Thread myThread2 = new Thread(new ParameterizedThreadStart(Print2)); //Same as above
+            myThread1.Start();
+            myThread2.Start(5);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Main Thread: {i}");
+                Thread.Sleep(300);
+            }
+        }
+        static void Print1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"First Thread: {i}");
+                Thread.Sleep(400);
+            }
+        }
+        static void Print2(object a) //Only with object
+        {
+            for (int i = 0; i < (int)a; i++)
+            {
+                Console.WriteLine($"Second Thread: {i}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
