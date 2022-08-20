5c354eb95b9fbc988defa325887902f17a5b3039
VahagnVoskanyan <vahagnlvoskanyan@mail.ru>
2022-08-20 14:29:04 +0400

    additions

--- Threads/Program.cs
+++ Threads/Program.cs
@@ -8,6 +8,9 @@
             //Thread myThread1 = new Thread(new ThreadStart(Print1)); //Same as above
             Thread myThread2 = new Thread(Print2); //ParameterizedThreadStart delegate
             //Thread myThread2 = new Thread(new ParameterizedThreadStart(Print2)); //Same as above
+            Person person = new Person("Zaven",28);
+            Thread myThread3 = new Thread(Print3);
+            myThread3.Start(person); //With multiply arguments
             myThread1.Start();
             myThread2.Start(5);
             for (int i = 0; i < 5; i++)
@@ -24,12 +27,38 @@
                 Thread.Sleep(400);
             }
         }
-        static void Print2(object a) //Only with object
+        static void Print2(object a) //Only with object type argument
         {
             for (int i = 0; i < (int)a; i++)
             {
                 Console.WriteLine($"Second Thread: {i}");
                 Thread.Sleep(500);
+            }
+        }
+        class Person
+        {
+            public Person(string name, int age)
+            {
+                Name = name;
+                Age = age;
+            }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public void Print()
+            {
+                Console.WriteLine($"Name: {Name}");
+                Console.WriteLine($"Age: {Age}");
+            }
+        }
+        static void Print3(object a) 
+        {
+            if (a is Person person)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    person.Print();
+                    Thread.Sleep(600);
+                }
             }
         }
     }
