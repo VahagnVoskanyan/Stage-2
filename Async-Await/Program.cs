6b53a4b8ce28cae179e12c3b92c8bb42a5b7ca4d
VahagnVoskanyan <vahagnlvoskanyan@mail.ru>
2022-08-21 22:56:47 +0400

    new

---
+++ Async-Await/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Async_Await
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = MethodAsync();
+            for (int i = 0; i < 200; i++)
+            {
+                Console.WriteLine("Maaaaaaaaaaaaaaaaain: " + i);
+            }
+            task.Wait(); //Waiting for async method ending before program ending
+        }
+        async static Task MethodAsync()
+        {
+            await Task.Run(() =>  //Main doesn't wait this to end
+            {
+                for (int i = 0; i < 300; i++)
+                {
+                    Console.WriteLine("Method: " + i);
+                }
+                Console.WriteLine("Methooooooooooooooooooooooood end.");
+            });
+        }
+    }
+}
