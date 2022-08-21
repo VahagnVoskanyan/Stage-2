a2d6b1feff7e0d12697b38b28398a15172d71458
VahagnVoskanyan <vahagnlvoskanyan@mail.ru>
2022-08-21 23:17:59 +0400

    test

--- Async-Await/Program.cs
+++ Async-Await/Program.cs
@@ -19,6 +19,7 @@
                 for (int i = 0; i < 220; i++)
                 {
                     await Task.Delay(10); //Doesn't work without 'await'
+                    //Thread.Sleep(10); //Similar with above but they aren't the same
                     Console.WriteLine("Method: " + i);
                 }
                 Console.WriteLine("Methooooooooooooooooooooooood end.");
