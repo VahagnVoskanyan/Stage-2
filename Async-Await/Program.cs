13ad7f7c857cbee9d91bb8b0150d269a0c0a8f5d
VahagnVoskanyan <vahagnlvoskanyan@mail.ru>
2022-08-21 23:13:32 +0400

    test

--- Async-Await/Program.cs
+++ Async-Await/Program.cs
@@ -7,16 +7,18 @@
             Task task = MethodAsync();
             for (int i = 0; i < 200; i++)
             {
+                Thread.Sleep(10); //This doesn't concern MethodAsync()
                 Console.WriteLine("Maaaaaaaaaaaaaaaaain: " + i);
             }
             task.Wait(); //Waiting for async method ending before program ending
         }
         async static Task MethodAsync()
         {
-            await Task.Run(() =>  //Main doesn't wait this to end
+            await Task.Run(async () =>  //Main doesn't wait this to end
             {
-                for (int i = 0; i < 300; i++)
+                for (int i = 0; i < 220; i++)
                 {
+                    await Task.Delay(10); //Doesn't work without 'await'
                     Console.WriteLine("Method: " + i);
                 }
                 Console.WriteLine("Methooooooooooooooooooooooood end.");
